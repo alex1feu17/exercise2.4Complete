5786d0135870475d9df80ccc204326ade969da5f
alex1feu17 <alexyd@hotmail.ca>
2020-01-28 20:06:59 -0500

    exercise2.3 Complete

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        public static bool IsPrime(int n)
+        {
+            int x = (int)Math.Floor(Math.Sqrt(n));
+            if(n==1)
+            {
+                return false;
+            }
+            else
+            {
+                if(n==2)
+                {
+                    return true;
+
+                }
+                else
+                {
+                    int i = 2;
+                    if(i<=x)
+                    {
+                        while(i <= x)
+                        {
+
+                        
+                            if(n%i==0)
+                            {
+                                return false;
+
+                            }
+                            else
+                            {
+                                ++i;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Sum of the first 500 prime numbers:");
+            long sum = 0;
+            int ctr = 0;
+            int n = 2;
+
+            if(ctr<500)
+            {
+                while(ctr < 500)
+                {
+                    if(IsPrime(n)==true)
+                    {
+                        sum += n;
+                        n++;
+                    }
+                    else
+                    {
+                        n++;
+                    }
+                }
+                
+            }
+            else 
+            {
+                Console.WriteLine(sum.ToString());
+            }
+        }
+    }
+}
